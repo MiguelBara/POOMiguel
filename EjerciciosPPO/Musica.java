2d080d10fcbbeae25a43f82c7c513efaab2ed298
MiguelBarajas <miguelbara2032@gmail.com>
2024-09-11 12:00:13 -0500

    probando
    volvi a empezar varios ejercicios por un problema con el package

---
+++ EjerciciosPPO/Musica.java
@@ -0,0 +1,10 @@
+package EjerciciosPPO;
+
+public class Musica {
+
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        System.out.println("probando fucking java");
+    }
+
+}
