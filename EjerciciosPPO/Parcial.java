467dbee348f980a658e70e5343f690279882f4d0
MiguelBarajas <miguelbara2032@gmail.com>
2024-09-19 10:26:59 -0500

    Update Parcial.java

--- EjerciciosPPO/Parcial.java
+++ EjerciciosPPO/Parcial.java
@@ -1,71 +1,71 @@
 package EjerciciosPPO;
 
 import java.util.Random;
+import java.util.Scanner;
 
+/*@ Miguel Barajas 20241222299 */
 public class Parcial {
 
     public static void main(String[] args) {
         int numHabitaciones = 10;
-        int[][] matrix = generateMatrix(numHabitaciones);
-
-        System.out.println("Matriz generada:");
-        for (int i = 0; i < numHabitaciones; i++) {
-            for (int j = 0; j < 7; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        int numHabitacion = Integer.parseInt(System.console().readLine("Ingrese el número de habitación para ver el reporte: "));
-        printReport(matrix, numHabitacion);
-
-        generateStatistics(matrix);
-    }
-
-    public static int[][] generateMatrix(int numHabitaciones) {
-        int[][] matrix = new int[numHabitaciones][7];
+        int[][] matriz = new int[numHabitaciones][7];
         Random random = new Random();
 
         for (int i = 0; i < numHabitaciones; i++) {
             for (int j = 0; j < 7; j++) {
                 if (j == 2) { // Miércoles con promoción
-                    matrix[i][j] = random.nextInt(61) + 40; // 40-100
+                    matriz[i][j] = random.nextInt(61) + 40; // 40-100
                 } else if (j >= 4) { // Fines de semana (viernes, sábado, domingo)
-                    matrix[i][j] = random.nextInt(61) + 36; // 20% más que lunes
+                    matriz[i][j] = random.nextInt(61) + 36; // 20% más que lunes
                 } else {
-                    matrix[i][j] = random.nextInt(51) + 30; // 30-80
+                    matriz[i][j] = random.nextInt(51) + 30; // 30-80
                 }
             }
         }
-        return matrix;
-    }
 
-    public static void printReport(int[][] matrix, int numHabitacion) {
-        int[] habitacion = matrix[numHabitacion - 1];
+        System.out.println("Matriz generada:");
+        for (int i = 0; i < numHabitaciones; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int numHabitacion;
+
+        while (true) {
+            System.out.print("Ingrese el número de habitación (1-" + numHabitaciones + "): ");
+            numHabitacion = scanner.nextInt();
+
+            if (numHabitacion < 1 || numHabitacion > numHabitaciones) {
+                System.out.println("Error: El número de habitación debe estar entre 1 y " + numHabitaciones);
+            } else {
+                break;
+            }
+        }
 
         System.out.println("No. habitación " + numHabitacion);
         System.out.println("Consumo diario:");
-        System.out.println("Lunes: " + habitacion[0]);
-        System.out.println("Martes: " + habitacion[1]);
-        System.out.println("Miércoles: " + habitacion[2]);
-        System.out.println("Jueves: " + habitacion[3]);
-        System.out.println("Viernes: " + habitacion[4]);
-        System.out.println("Sábado: " + habitacion[5]);
-        System.out.println("Domingo: " + habitacion[6]);
+        System.out.println("Lunes: " + matriz[numHabitacion - 1][0]);
+        System.out.println("Martes: " + matriz[numHabitacion - 1][1]);
+        System.out.println("Miércoles: " + matriz[numHabitacion - 1][2]);
+        System.out.println("Jueves: " + matriz[numHabitacion - 1][3]);
+        System.out.println("Viernes: " + matriz[numHabitacion - 1][4]);
+        System.out.println("Sábado: " + matriz[numHabitacion - 1][5]);
+        System.out.println("Domingo: " + matriz[numHabitacion - 1][6]);
 
-        double promedioSemana = (habitacion[0] + habitacion[1] + habitacion[2] + habitacion[3]) / 4.0;
-        double promedioFinDeSemana = (habitacion[4] + habitacion[5] + habitacion[6]) / 3.0;
+        double promedioSemana = (matriz[numHabitacion - 1][0] + matriz[numHabitacion - 1][1] + matriz[numHabitacion - 1][2] + matriz[numHabitacion - 1][3]) / 4.0;
+        double promedioFinDeSemana = (matriz[numHabitacion - 1][4] + matriz[numHabitacion - 1][5] + matriz[numHabitacion - 1][6]) / 3.0;
 
         System.out.printf("Promedio entre semana: %.2f%n", promedioSemana);
         System.out.printf("Promedio fin de semana: %.2f%n", promedioFinDeSemana);
-    }
 
-    public static void generateStatistics(int[][] matrix) {
         int[] consumoTotalPorDia = new int[7];
 
-        for (int i = 0; i < matrix.length; i++) {
+        for (int i = 0; i < numHabitaciones; i++) {
             for (int j = 0; j < 7; j++) {
-                consumoTotalPorDia[j] += matrix[i][j];
+                consumoTotalPorDia[j] += matriz[i][j];
             }
         }
 
@@ -75,18 +75,23 @@ public class Parcial {
         }
 
         double consumoPromedioPorHabitacion = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            consumoPromedioPorHabitacion += sumArray(matrix[i]);
+        for (int i = 0; i < numHabitaciones; i++) {
+            for (int j = 0; j < 7; j++) {
+                consumoPromedioPorHabitacion += matriz[i][j];
+            }
         }
-        consumoPromedioPorHabitacion /= matrix.length;
+        consumoPromedioPorHabitacion /= numHabitaciones * 7;
 
         System.out.printf("Consumo promedio por habitación: %.2f%n", consumoPromedioPorHabitacion);
 
         int maxConsumo = Integer.MIN_VALUE;
         int minConsumo = Integer.MAX_VALUE;
 
-        for (int i = 0; i < matrix.length; i++) {
-            int consumoHabitacion = sumArray(matrix[i]);
+        for (int i = 0; i < numHabitaciones; i++) {
+            int consumoHabitacion = 0;
+            for (int j = 0; j < 7; j++) {
+                consumoHabitacion += matriz[i][j];
+            }
             if (consumoHabitacion > maxConsumo) {
                 maxConsumo = consumoHabitacion;
             }
@@ -97,13 +102,5 @@ public class Parcial {
 
         System.out.println("Habitación que más consume: " + maxConsumo);
         System.out.println("Habitación que menos consume: " + minConsumo);
-    }
-
-    public static int sumArray(int[] array) {
-        int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            sum += array[i];
-        }
-        return sum;
     }
 }
