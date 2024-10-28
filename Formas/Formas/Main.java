31ceffc8f977fe018e13d13d6e62b1f0ac783d08
Miguel Angel Barajas Bustos <u20241222299@usco.edu.co>
2024-10-28 14:55:28 -0500

    ejercicio 8.a completo

--- Formas/Formas/Main.java
+++ Formas/Formas/Main.java
@@ -2,14 +2,40 @@ package Formas;
 
 public class Main {
     public static void main(String[] args) {
+        // Crear un array de formas
+        Forma[] formas = new Forma[4];
 
-        Cuadrado cuadrado1 = new Cuadrado("c1", 0, 0, "azul", 3);
-        System.out.println("El area de: " + cuadrado1.getNombre() + "es :" + cuadrado1.calcularArea());
-        System.out.println("El perimetro de: " + cuadrado1.getNombre() + "es :" + cuadrado1.calcularPerimetro());
+        // Crear 2 cuadrados
+        formas[0] = new Cuadrado("Cuadrado 1", 0, 0, "Rojo", 4);
+        formas[1] = new Cuadrado("Cuadrado 2", 1, 1, "Verde", 5);
 
-        Triangulo triangulo1 = new Triangulo("t1", 0, 0, "verde", 4, 6);
-        System.out.println("El area de: " + triangulo1.getNombre() + "es :" + triangulo1.calcularArea());
+        // Crear 2 rectángulos
+        formas[2] = new Rectangulo("Rectángulo 1", 2, 2, "Azul", 3, 6);
+        formas[3] = new Rectangulo("Rectángulo 2", 3, 3, "Amarillo", 4, 8);
 
+        // Calcular la suma de las áreas de todas las formas
+        double sumaAreas = 0;
+        double sumaAreasCuadrados = 0;
+        int contadorCuadrados = 0;
+
+        for (Forma forma : formas) {
+            double area = forma.calcularArea();
+            sumaAreas += area;
+
+            // Verificar si la forma es un cuadrado
+            if (forma instanceof Cuadrado) {
+                sumaAreasCuadrados += area;
+                contadorCuadrados++;
+            }
+        }
+
+        // Mostrar resultados
+        System.out.println("Suma de las areas de todas las formas: " + sumaAreas);
+        if (contadorCuadrados > 0) {
+            double mediaAreasCuadrados = sumaAreasCuadrados / contadorCuadrados;
+            System.out.println("Media de las areas de los cuadrados: " + mediaAreasCuadrados);
+        } else {
+            System.out.println("No hay cuadrados en el array.");
+        }
     }
-
 }
